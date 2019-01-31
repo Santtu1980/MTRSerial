fc6f2cd9d87ed8a29ad3f109f4f479ccfd20d4f1
Samuli <samuli.lahdenpera@ocuspecto.com>
2019-01-31 21:10:04 +0200

    Creating communication.

--- MTRSerial/MTRSerial.cs
+++ MTRSerial/MTRSerial.cs
@@ -49,26 +49,210 @@ namespace MTRSerial
         private long _lastAcknowledged_ms = 0;
         private SerialPort _serialPort;
         private volatile bool _waitingCommunicationCheckAck;
-        private readonly TimerCallback _timerCallbackDelegate;
         private long _lastCommandSent;
-        
+        private DateTime _acknowledgeFromMTRDateTime = DateTime.MinValue;
+
         public MTRSerialPort()
         {
-            _timerCallbackDelegate = KeepAliveTimerElapsed;
         }
 
+        /// <summary>
+        /// Returns the serial communication starting time (UTC)
+        /// </summary>
         public DateTime GetPowerUpTime_utc => _powerUpTime_utc.ToUniversalTime();
 
+        /// <summary>
+        /// Send command to the MTR
+        /// </summary>
+        /// <param name="command">Command to send</param>
+        /// <param name="binary">Some commands needs extra binarydata. If it is needed and no binary data given an exception will throw</param>
         public void AskFromMTR(CommandsToMTR.CommandName command, byte[] binary = null)
         {
             var cmd = GetCommand(command, binary);
             SendData(cmd);
         }
 
+        /// <summary>
+        /// In some cases to "reboot" connection it need to close and reopen
+        /// </summary>
+        /// <returns></returns>
+        public bool CloseAndReopenSerialPort()
+        {
+            CloseSerialPort();
+            Thread.Sleep(500);// The best practice for any application is to wait for some amount of time after calling the Close method before attempting to call the Open method, as the port may not be closed instantly. From:https://msdn.microsoft.com/en-us/library/system.io.ports.serialport.close.aspx
+            return OpenSerialPort();
+        }
+
+        /// <summary>
+        /// Closes the Serial port and stop communication
+        /// </summary>
+        public void CloseSerialPort()
+        {
+            if(_serialPort != null && _serialPort.IsOpen)
+            {
+                try
+                {
+                    _serialPort.Close();
+                    if(MTRCommunication != null)
+                    {
+                        var infoArgs = CreateInfoArgs(@"Serial port closed");
+                        MTRCommunication(this, infoArgs);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    if(MTRCommunication != null)
+                    {
+                        var infoArgs = CreateInfoArgs(@"Serial port closing failed, check exception from application log");
+                        MTRCommunication(this, infoArgs);
+                    }
+                    // TODO Some logging
+                    //Logger.AddMessageToLogQueue(@"Serial port closing failed");
+                    //Logger.FlushLogQueue();
+                    return;
+                }
+            }
+
+            SerialPortClosed?.Invoke(this, EventArgs.Empty);
+        }
+        
+        /// <summary>
+        /// Opens the Serial port and enables the communication
+        /// </summary>
+        /// <returns></returns>
+        public bool OpenSerialPort()
+        {
+            // Init the serial port and communications log
+            var success = InitSerialPort();
+            if(_serialPort != null && success)
+            {
+                MTRResponseData.Reset();
+                ResetCommunicationErrorsCount();
+
+                SerialPortOpened?.Invoke(this, EventArgs.Empty);
+
+                if(MTRCommunication != null)
+                {
+                    var infoArgs = CreateInfoArgs(@"Serial port opened");
+                    MTRCommunication(this, infoArgs);
+                }
+            }
+            return success;
+        }
+
+        /// <summary>
+        /// The amount of communication errors increase if there occurs some errors. This method returns the amount of the errors. It can be reset by using "ResetCommunicationErrorsCount"
+        /// </summary>
+        /// <returns></returns>
+        public int GetAmountOfCommunicationErrors()
+        {
+            return _communicationErrorsCount;
+        }
+        
+        /// <summary>
+        /// This resets the number of the communication errors
+        /// </summary>
+        private void ResetCommunicationErrorsCount()
+        {
+            _communicationErrorsCount = 0;
+            CommsErrorCountChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// This returns true if the the Serial port connection is open
+        /// </summary>
+        /// <returns>True if open, otherwise false</returns>
+        public bool IsSerialPortOpen()
+        {
+            return _serialPort != null && _serialPort.IsOpen;
+        }
+
+        /// <summary>
+        /// Event handler when some data received from the serial port
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(Thread.CurrentThread.Name))
+            {
+                Thread.CurrentThread.Name = "SerialPortRXThread";
+                Thread.CurrentThread.Priority = ThreadPriority.AboveNormal;
+            }
+            lock(_commLock)
+            {
+                var threw = false;
+                while(!threw)
+                {
+                    try
+                    {
+                        ParseRxString(_serialPort.ReadLine());
+                    }
+                    catch
+                    {
+                        threw = true;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Just asking a status to check if connection is ok
+        /// </summary>
+        /// <returns><c>False</c> if  there isn't connection after 20 tries.<c>True</c> if there came some message</returns>
+        public bool EnsureCommunicationToMTR()
+        {
+            _waitingCommunicationCheckAck = true;
+            SendData(@"/ST"); 
+            var triesToWaitAck = 0;
+            while(triesToWaitAck < 20 && _waitingCommunicationCheckAck)
+            {
+                Thread.Sleep(100);
+                triesToWaitAck++;
+            }
+            return !_waitingCommunicationCheckAck;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="acknowledgedTime"></param>
+        /// <returns></returns>
+        public long GetMTRActiveTime(out DateTime acknowledgedTime)
+        {
+            var before = MTRResponseData.AcknowledgedTime_ms;
+            Thread.Sleep(1000);
+            SendData(@"/ST");
+            var waited = 0;
+            while(MTRResponseData.AcknowledgedTime_ms == before && waited < 2000)
+            {
+                Thread.Sleep(100);
+                waited += 100;
+            }
+
+            acknowledgedTime = _acknowledgeFromMTRDateTime;
+            return MTRResponseData.AcknowledgedTime_ms;
+        }
+
+        private void SendKeepAliveIfNecessary()
+        {
+            if(DateTime.Now.Ticks / DefaultValues.SystemTickDivider - _lastCommandSent > 10000 && IsSerialPortOpen())
+            {
+                SendData(@"/ST");
+            }
+        }
+
         private string GetCommand(CommandsToMTR.CommandName command, byte[] binary = null)
         {
-            if(command == CommandsToMTR.CommandName.SpoolBinary || command == CommandsToMTR.CommandName.GetMessageBinary && binary == null) throw new Exception("Binary content missing.")
-            switch (command)
+            if(command == CommandsToMTR.CommandName.SpoolBinary ||
+               command == CommandsToMTR.CommandName.GetMessageBinary ||
+               command == CommandsToMTR.CommandName.SetClock
+                && binary == null)
+            {
+                throw new Exception("Binary content missing.");
+            }
+
+            switch(command)
             {
                 case CommandsToMTR.CommandName.Status: return "/ST";
                 case CommandsToMTR.CommandName.Spool: return "/SA";
@@ -81,7 +265,7 @@ namespace MTRSerial
             }
         }
 
-        public virtual void SendData(string cmd)
+        private void SendData(string cmd)
         {
             if(string.IsNullOrEmpty(cmd)) return;
             if(cmd.Contains(@"/NS"))
@@ -123,176 +307,13 @@ namespace MTRSerial
                 MTRCommunication(this, eventArgs);
             }
         }
-
-        public bool CloseAndReopenSerialPort()
-        {
-            CloseSerialPort();
-            Thread.Sleep(500);// The best practice for any application is to wait for some amount of time after calling the Close method before attempting to call the Open method, as the port may not be closed instantly. From:https://msdn.microsoft.com/en-us/library/system.io.ports.serialport.close.aspx
-            return OpenSerialPort();
-        }
-
-        public void CloseSerialPort()
-        {
-            SendData(@"R0");
-            WaitForReply(@"R0");
-
-            if(_serialPort != null && _serialPort.IsOpen)
-            {
-                try
-                {
-                    _serialPort.Close();
-                    if(MTRCommunication != null)
-                    {
-                        var infoArgs = CreateInfoArgs(@"Serial port closed");
-                        MTRCommunication(this, infoArgs);
-                    }
-                }
-                catch(Exception ex)
-                {
-                    if(MTRCommunication != null)
-                    {
-                        var infoArgs = CreateInfoArgs(@"Serial port closing failed, check exception from application log");
-                        MTRCommunication(this, infoArgs);
-                    }
-                    // TODO Some logging
-                    //Logger.AddMessageToLogQueue(@"Serial port closing failed");
-                    //Logger.FlushLogQueue();
-                    return;
-                }
-            }
-            if(SerialPortClosed != null)
-            {
-                SerialPortClosed(this, EventArgs.Empty);
-            }
-        }
-
-        /// <summary>
-        /// Opens the serial (COM) port
-        /// </summary>
-        public bool OpenSerialPort()
-        {
-            // Init the serial port and communications log
-            var success = InitSerialPort();
-            if(_serialPort != null && success)
-            {
-                MTRResponseData.Reset();
-                ResetCommunicationErrorsCount();
-                _lastKeyPressFromTestStart_ms = 0;
-                if(SerialPortOpened != null)
-                {
-                    SerialPortOpened(this, EventArgs.Empty);
-                }
-                if(MTRCommunication != null)
-                {
-                    var infoArgs = CreateInfoArgs(@"Serial port opened");
-                    MTRCommunication(this, infoArgs);
-                }
-            }
-            return success;
-        }
-
-        public int GetCommunicationErrorsCount()
-        {
-            return _communicationErrorsCount;
-        }
         
-        public void ResetCommunicationErrorsCount()
-        {
-            _communicationErrorsCount = 0;
-            if(CommsErrorCountChanged != null)
-            {
-                CommsErrorCountChanged(this, EventArgs.Empty);
-            }
-        }
-
-        public bool SerialPortOpen()
-        {
-            return _serialPort != null && _serialPort.IsOpen;
-        }
-
-        public void DataReceived(object sender, SerialDataReceivedEventArgs e)
-        {
-            if(string.IsNullOrEmpty(Thread.CurrentThread.Name))
-            {
-                Thread.CurrentThread.Name = "SerialPortRXThread";
-                Thread.CurrentThread.Priority = ThreadPriority.AboveNormal;
-            }
-            lock(_commLock)
-            {
-                var threw = false;
-                while(!threw)
-                {
-                    try
-                    {
-                        ParseRxString(_serialPort.ReadLine());
-                    }
-                    catch
-                    {
-                        threw = true;
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// No operation, i.e. ping.
-        /// The device just responds with an ack in any mode.
-        /// This can be used by the PC to keep the Device in Active mode, if nothing is happening.
-        /// </summary>
-        /// <returns><c>False</c> if  there isnt connection after 20 tries.<c>True</c> if there is a connection</returns>
-        public bool EnsureCommunicationToPerimeter()
-        {
-            _waitingCommunicationCheckAck = true;
-            SendData(@"/ST"); 
-            int i = 0;
-            while(i < 20 && _waitingCommunicationCheckAck)
-            {
-                Thread.Sleep(100);
-                i++;
-            }
-            return !_waitingCommunicationCheckAck;
-        }
-
-        private DateTime _acknowledgeFromMTRDateTime = DateTime.MinValue;
-        public long GetMTRActiveTime(out DateTime acknowledgedTime)
-        {
-            var before = MTRResponseData.AcknowledgedTime_ms;
-            Thread.Sleep(1000);
-            SendData(@"Z");
-            int waited = 0;
-            while(MTRResponseData.AcknowledgedTime_ms == before && waited < 2000)
-            {
-                Thread.Sleep(100);
-                waited += 100;
-            }
-
-            acknowledgedTime = _acknowledgeFromMTRDateTime;
-            return MTRResponseData.AcknowledgedTime_ms;
-        }
-
-        private void KeepAliveTimerElapsed(Object stateInfo)
-        {
-            SendKeepAliveIfNecessary();
-        }
-
-        private void SendKeepAliveIfNecessary()
-        {
-            if(DateTime.Now.Ticks / DefaultValues.SystemTickDivider - _lastCommandSent > 10000 && SerialPortOpen())
-            {
-                SendData(@"Z");
-            }
-        }
-
-        /// <summary>
-        /// Wait max 200ms for the reply.
-        /// And increase the communication errors count
-        /// </summary>
         private void WaitForReply(string cmd)
         {
-            var wait_count = 0;
+            var waitCount = 0;
             while(_waitAck)
             {
-                if(wait_count++ >= 200 && cmd.Length >= 1)
+                if(waitCount++ >= 200 && cmd.Length >= 1)
                 {
                     _waitAck = false;
                     IncreaseCommunicationErrorsCount();
@@ -301,22 +322,12 @@ namespace MTRSerial
                 Thread.Sleep(1);
             }
         }
-
-        private void ReportHardwareError()
-        {
-            if(HardwareErrorOccurred != null)
-            {
-                HardwareErrorOccurred(this, EventArgs.Empty);
-            }
-        }
-
+        
         private void IncreaseCommunicationErrorsCount()
         {
             _communicationErrorsCount++;
-            if(CommsErrorCountChanged != null)
-            {
-                CommsErrorCountChanged(this, EventArgs.Empty);
-            }
+            CommsErrorCountChanged?.Invoke(this, EventArgs.Empty);
+
             if(MTRCommunication != null)
             {
                 var infoArgs = CreateInfoArgs(string.Format(@"Communication error count increased, count is now {0}", _communicationErrorsCount));
@@ -470,7 +481,7 @@ namespace MTRSerial
         private bool InitSerialPort()
         {
             // LOGException serialPort.IsOpen
-            if(_serialPort != null && _serialPort.IsOpen && EnsureCommunicationToPerimeter())
+            if(_serialPort != null && _serialPort.IsOpen && EnsureCommunicationToMTR())
                 return true;
 
             // LOGException serialPort.IsOpen
@@ -508,7 +519,7 @@ namespace MTRSerial
                     {
                         _serialPort.DataReceived += DataReceived;
                         _serialPort.ErrorReceived += SerialPortErrorReceived;
-                        success = EnsureCommunicationToPerimeter();
+                        success = EnsureCommunicationToMTR();
                     }
                     if(!success && _serialPort.IsOpen)
                     {
